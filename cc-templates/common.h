ac0bc40b344b1cd626e861e6193084e32047dcf5
Connor Weeks <cweeks314@gmail.com>
2016-11-03 20:48:13 -0400

    Fixed incomplete documentation

--- cc-templates/common.h
+++ cc-templates/common.h
@@ -6,21 +6,26 @@
 #define META_MACHINES_COMMON_H
 
 /**
- * This value will be used for any
+ * This value will be used for any null or invalid value
  */
 #define NULL_VALUE  (-1)
 
+
+/**
+ * Specifies a null type, which may be used
+ * to terminate a list.
+ */
+struct NullType {};
 
 /**
  * This data structure specifies a recursively defined list.
  * Each element contains an integer value and a 'tail',
  * representing a list containing the next elements.
  * Fpr example, the list {1, 2, 3, 4} ,ay be defined
- * as follows: List<
+ * as follows: List<1, List<2, List<3, List<4, NullType> > > >
  */
 template<int current, typename tail>
 struct List {
-
     /**
      * The next sub-list, or NullList if no elemnts remain.
      */
@@ -33,12 +38,6 @@ struct List {
         value = current
     };
 };
-
-/**
- * Specifies a null type, which may be used
- * to terminate a list.
- */
-struct NullType {};
 
 
 /**
